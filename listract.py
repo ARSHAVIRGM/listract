0ae7f34d74749370a587fd7ef9ad0433255cac5e
arshavir_gm <76659587+ARSHAVIRGM@users.noreply.github.com>
2022-05-05 13:23:21 +0430

    Add files via upload

--- listract.py
+++ listract.py
@@ -11,6 +11,7 @@
 
 
 def extract(list, to):
+
     for x in range(len(list)):
 
         is_list = type(list[x]) == list
@@ -34,3 +35,53 @@ def extract(list, to):
 
             except :
                 to.append(list[x])
+
+def merge(list1 , list2 , to ):
+
+    for x in range(len(list1)):
+
+        is_list1 = type(list1[x]) == list1
+
+        if is_list1 == True:
+
+            for y in range(len(list1[x])):
+
+                mov = list1[x][y]
+
+                try :
+                    to.index(mov)
+
+                except :
+                    to.append(mov)
+        else:
+
+            try :
+            
+                to.index(list1[x])
+
+            except :
+                to.append(list1[x])
+
+    for x in range(len(list2)):
+
+        is_list2 = type(list2[x]) == list2
+
+        if is_list2 == True:
+
+            for y in range(len(list2[x])):
+
+                mov = list2[x][y]
+
+                try :
+                    to.index(mov)
+
+                except :
+                    to.append(mov)
+        else:
+
+            try :
+            
+                to.index(list2[x])
+
+            except :
+                to.append(list2[x])
